0ff74f2021e571850aa3c05d4d6cb051778ffd90
souza <jgmicroshs@gmail.com>
2018-02-24 18:22:12 -0300

    alterando e criando arquivos

---
+++ backup_banco.sql
@@ -0,0 +1,6 @@
+<h4> No Windows 8.x, escolha Área de trabalho. ...</h4>
+<h4> Abra o Painel de controle.</h4>
+<h4> Escolha Opções de pasta em Aparência e personalização.</h4>
+<h4> Clique no ícone Opções à direita da faixa de opções.</h4>
+<h4> Na caixa de diálogo Opções de pasta, selecione a guia Exibir.</h4>
+<h4> Selecione Mostrar arquivos, pastas e unidades ocultas.</h4>
