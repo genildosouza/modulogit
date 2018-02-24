b1f53c885411f71b8a0986525e581a049c73c093
souza <jgmicroshs@gmail.com>
2018-02-24 17:39:37 -0300

    modificando arquivos

--- index.php
+++ index.php
@@ -8,5 +8,7 @@
 
     <p>1 de mai de 2017 - No Windows 10, clique no botão Iniciar. Abra o Painel de controle. Escolha Opções de pasta em Aparência e personalização. Clique no ícone Opções à direita da faixa de opções. Na caixa de diálogo Opções de pasta, selecione a guia Exibir. Selecione Mostrar arquivos, pastas e unidades ocultas.</p>
 
+    <p>1 de mai de 2017 - No Windows 10, clique no botão Iniciar. Abra o Painel de controle. Escolha Opções de pasta em Aparência e personalização. Clique no ícone Opções à direita da faixa de opções. Na caixa de diálogo Opções de pasta, selecione a guia Exibir. Selecione Mostrar arquivos, pastas e unidades ocultas.</p>
+
   </body>
 </html>
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 Este é o arquivo readme do primeiro sistema.
+
+Este é o arquivo readme do primeiro sistema.
+
+Este é o arquivo readme do primeiro sistema.
